6ec603f5c64401dd4122829ca29b5cd8e838cea4
John Bridges <gamerjohnnyb@gmail.com>
2025-01-16 15:15:52 -0500

    f

--- playbutton.js
+++ playbutton.js
@@ -1,6 +1,25 @@
-const button = document.getElementById("firstButton");
-button.addEventListener("click", () => {
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    
+    document.getElementById("firstButton").addEventListener("click", () => {
+      console.log(window.location.href);
+      window.location.href = "loggedin.html";
+    });
+
+  document.getElementById("play").addEventListener("click", () => {
     console.log(window.location.href);
-    window.location.href = "loggedin.html";
+    window.location.href = "playStage.html";
   });
-  
+
+  document.getElementById("lead").addEventListener("click", () => {
+    console.log(window.location.href);
+    window.location.href = "leaderpage.html";
+  });
+
+  document.getElementById("accSett").addEventListener("click", () => {
+    console.log(window.location.href);
+    window.location.href = "accountSetts.html";
+  });
+
+});
