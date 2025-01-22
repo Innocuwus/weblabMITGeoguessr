2a8e8bb52b8f513dcc555e4b57269da3f1f88dd2
John Bridges <gamerjohnnyb@gmail.com>
2025-01-22 00:04:19 -0500

    new start

--- skeleton-main/skeleton-main/server/renderComp.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-
-const renderComponent = (Component) => {
-  return ReactDOMServer.renderToString(React.createElement(Component));
-};
-
-module.exports = renderComponent;
